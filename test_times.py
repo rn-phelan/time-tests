d55719e0a22fc0c5764861d6b2ec20a5666fec13
Rebecca Phelan <rebecca.phelan.21@ucl.ac.uk>
2024-10-28 10:36:32 +0000

    Add tests for ex 12 - no overlap, touching times

--- test_times.py
+++ test_times.py
@@ -6,3 +6,23 @@ def test_given_input():
     result = compute_overlap_time(large, short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
+
+def test_compute_overlap_time():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 12:30:00", "2010-01-12 12:45:00")
+    # for a,b in compute_overlap_time(large, short):
+    #     assert b > a,  "There is no overlap time, so incorrectly returns"
+    expected=[] # empty list for no overlap
+    result = compute_overlap_time(large, short)
+    assert result == expected
+
+
+def test_touching_times():
+    large = time_range("2010-01-12 10:00:00", "2010-01-12 12:00:00")
+    short = time_range("2010-01-12 12:00:00", "2010-01-12 12:45:00")
+    # result = compute_overlap_time(large, short)
+    # for a,b in compute_overlap_time(large, short):
+    #     assert a==b,  "There is no overlap time, so incorrectly returns"
+    expected=[] # empty list for no overlap, just touching
+    result = compute_overlap_time(large, short)
+    assert result == expected
